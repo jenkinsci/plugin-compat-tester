42fc836a67ed06464427b448653d0405d2e4f6b8
Frederic Camblor <fcamblor@gmail.com>
2011-04-19 23:27:55 +0200

    added executeGoals()

--- src/main/java/org/jenkins/tools/test/model/MavenPom.java
+++ src/main/java/org/jenkins/tools/test/model/MavenPom.java
@@ -1,6 +1,9 @@
 package org.jenkins.tools.test.model;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.xml.transform.Result;
 import javax.xml.transform.Source;
@@ -9,7 +12,15 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
+import hudson.maven.MavenEmbedder;
+import hudson.maven.MavenEmbedderException;
+import hudson.maven.MavenRequest;
 import org.apache.commons.io.FileUtils;
+import org.apache.maven.execution.AbstractExecutionListener;
+import org.apache.maven.execution.ExecutionEvent;
+import org.apache.maven.execution.MavenExecutionResult;
+import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
+import org.jenkins.tools.test.exception.PomExecutionException;
 import org.jenkins.tools.test.exception.PomTransformationException;
 import org.springframework.core.io.ClassPathResource;
 
@@ -48,4 +59,38 @@ public class MavenPom {
 		}
 		
 	}
+
+    public MavenExecutionResult executeGoals(List goals) throws PomExecutionException {
+        final List<String> succeededPlugins = new ArrayList<String>();
+        MavenRequest mavenRequest = new MavenRequest();
+        //mavenRequest.setPom(pluginCheckoutDir.getAbsolutePath()+"/pom.xml");
+        mavenRequest.setBaseDirectory(rootDir.getAbsolutePath());
+        mavenRequest.setGoals(goals);
+        AbstractExecutionListener mavenListener = new AbstractExecutionListener(){
+            public void mojoSucceeded(ExecutionEvent event){
+                 succeededPlugins.add(event.getMojoExecution().getArtifactId());
+            }
+        };
+        mavenRequest.setExecutionListener(mavenListener);
+        mavenRequest.getUserProperties().put( "failIfNoTests", "false" );
+        mavenRequest.setPom(rootDir.getAbsolutePath()+"/pom.xml");
+
+        MavenExecutionResult result;
+        try {
+            MavenEmbedder embedder = new MavenEmbedder(Thread.currentThread().getContextClassLoader(), mavenRequest);
+            result = embedder.execute(mavenRequest);
+        }catch(MavenEmbedderException e){
+            // TODO: better manage this exception
+            throw new RuntimeException("Error during maven embedder execution", e);
+        } catch(ComponentLookupException e){
+            // TODO: better manage this exception
+            throw new RuntimeException("Error during maven embedder execution", e);
+        }
+
+        if(!result.getExceptions().isEmpty()){
+            throw new PomExecutionException("Error while executing pom goals : "+ Arrays.toString(goals.toArray()), result.getExceptions(), succeededPlugins);
+        }
+
+        return result;
+    }
 }
