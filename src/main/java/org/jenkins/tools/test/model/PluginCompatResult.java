e83ed65ec37f03971cc1117e55c73e3511ed965e
Frederic Camblor <fcamblor@gmail.com>
2011-04-20 23:33:14 +0200

    refactored PluginCompatResult with new data
    Conflicts:
    
    	src/main/java/org/jenkins/tools/test/model/PluginCompatResult.java

--- src/main/java/org/jenkins/tools/test/model/PluginCompatResult.java
+++ src/main/java/org/jenkins/tools/test/model/PluginCompatResult.java
@@ -1,31 +1,45 @@
 package org.jenkins.tools.test.model;
 
-/**
- * Created by IntelliJ IDEA.
- * User: Fred
- * Date: 20/04/11
- * Time: 00:38
- * To change this template use File | Settings | File Templates.
- */
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+import java.util.Date;
+
 public class PluginCompatResult {
-    public final String coreGroupId;
-    public final String coreArtifactId;
-    public final String coreVersion;
+    public final String pluginName;
+    public final String pluginVersion;
+    public final String pluginUrl;
 
     public final boolean compilationOk;
     public final boolean testsOk;
 
+    public final Date compatTestExecutedOn;
+
     public final String errorMessage;
 
-    public PluginCompatResult(String coreGroupId, String coreArtifactId, String coreVersion,
+    public PluginCompatResult(String pluginName, String pluginVersion, String pluginUrl,
                               boolean compilationOk, boolean testsOk, String errorMessage){
-        this.coreGroupId = coreGroupId;
-        this.coreArtifactId = coreArtifactId;
-        this.coreVersion = coreVersion;
+        this.pluginName = pluginName;
+        this.pluginVersion = pluginVersion;
+        this.pluginUrl = pluginUrl;
 
         this.compilationOk = compilationOk;
         this.testsOk = testsOk;
 
         this.errorMessage = errorMessage;
+
+        this.compatTestExecutedOn = new Date(); // now !
+    }
+
+    public boolean equals(Object o){
+        if(o==null || !(o instanceof PluginCompatResult)){
+            return false;
+        }
+        PluginCompatResult res = (PluginCompatResult)o;
+        return new EqualsBuilder().append(pluginName, res.pluginName).append(pluginVersion, res.pluginVersion).isEquals();
+    }
+
+    public int hashCode(){
+        return new HashCodeBuilder().append(pluginName).append(pluginVersion).hashCode();
     }
 }
