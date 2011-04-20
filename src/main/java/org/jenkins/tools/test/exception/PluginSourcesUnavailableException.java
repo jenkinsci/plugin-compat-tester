177b1dc4820f565ba6bb54a44300325a5b343a8e
Frederic Camblor <fcamblor@gmail.com>
2011-04-21 00:02:39 +0200

    creared submodule plugins-compat-tester

--- src/main/java/org/jenkins/tools/test/exception/PluginSourcesUnavailableException.java
+++
@@ -1,8 +0,0 @@
-package org.jenkins.tools.test.exception;
-
-public class PluginSourcesUnavailableException extends Exception {
-
-	public PluginSourcesUnavailableException(String message, Throwable cause){
-		super(message, cause);
-	}
-}
--- src/main/java/org/jenkins/tools/test/exception/PomTransformationException.java
+++
@@ -1,8 +0,0 @@
-package org.jenkins.tools.test.exception;
-
-public class PomTransformationException extends Exception {
-
-	public PomTransformationException(String message, Throwable cause){
-		super(message, cause);
-	}
-}
