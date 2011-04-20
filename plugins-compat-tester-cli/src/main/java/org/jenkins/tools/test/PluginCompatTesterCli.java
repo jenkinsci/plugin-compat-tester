06c99c76c75ae2725727758445546ea38dd9f681
Frederic Camblor <fcamblor@gmail.com>
2011-04-21 01:19:00 +0200

    provided implementation for PluginCompatTesterCli

---
+++ plugins-compat-tester-cli/src/main/java/org/jenkins/tools/test/PluginCompatTesterCli.java
@@ -0,0 +1,45 @@
+package org.jenkins.tools.test;
+
+import org.codehaus.plexus.PlexusContainerException;
+import org.jenkins.tools.test.model.PluginCompatTesterConfig;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class PluginCompatTesterCli {
+
+    public static void main(String[] args) throws IOException, PlexusContainerException {
+        String updateCenterUrl = System.getProperty("updateCenterUrl");
+        String parentCoord = System.getProperty("parentCoordinates");
+        String workDirectory = System.getProperty("workDirectory");
+        String reportFile = System.getProperty("reportFile");
+        String pluginsListParam = System.getProperty("pluginsList");
+
+        if(workDirectory == null){
+            throw new IllegalArgumentException("Parameter -DworkDirectory should be passed to the CLI !");
+        }
+        if(!new File(workDirectory).exists()){
+            throw new IllegalArgumentException("Parameter -DworkDirectory should be referencing an existing directory !\");");
+        }
+        if(reportFile == null){
+            throw new IllegalArgumentException("Parameter -DreportFile should be passed to the CLI !");
+        }
+
+        PluginCompatTesterConfig config = null;
+        if(updateCenterUrl != null || parentCoord != null){
+            config = new PluginCompatTesterConfig(updateCenterUrl, parentCoord, new File(workDirectory), new File(reportFile));
+        } else {
+            config = new PluginCompatTesterConfig(new File(workDirectory), new File(reportFile));
+        }
+
+        if(pluginsListParam != null){
+            List<String> pluginsList = Arrays.asList(pluginsListParam.split(","));
+            config.setPluginsList(pluginsList);
+        }
+
+        PluginCompatTester tester = new PluginCompatTester(config);
+        tester.testPlugins();
+    }
+}
