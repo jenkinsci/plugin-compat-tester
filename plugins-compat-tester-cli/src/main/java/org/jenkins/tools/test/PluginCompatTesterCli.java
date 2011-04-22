069e90efb11ab8b99caa76b264da99c64f4e8d12
Frederic Camblor <fcamblor@gmail.com>
2011-04-22 20:51:31 +0200

    m2SettingsFile is now a mandatory parameter + deleted packaged m2-settings.xml file

--- plugins-compat-tester-cli/src/main/java/org/jenkins/tools/test/PluginCompatTesterCli.java
+++ plugins-compat-tester-cli/src/main/java/org/jenkins/tools/test/PluginCompatTesterCli.java
@@ -2,7 +2,6 @@ package org.jenkins.tools.test;
 
 import org.codehaus.plexus.PlexusContainerException;
 import org.jenkins.tools.test.model.PluginCompatTesterConfig;
-import org.springframework.core.io.ClassPathResource;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,34 +13,35 @@ public class PluginCompatTesterCli {
     public static void main(String[] args) throws IOException, PlexusContainerException {
         String updateCenterUrl = System.getProperty("updateCenterUrl");
         String parentCoord = System.getProperty("parentCoordinates");
-        String workDirectory = System.getProperty("workDirectory");
+        String workDirectoryPath = System.getProperty("workDirectory");
         String reportFile = System.getProperty("reportFile");
         String pluginsListParam = System.getProperty("pluginsList");
         String m2SettingsFilePath = System.getProperty("m2SettingsFile");
 
-        if(workDirectory == null){
+        if(workDirectoryPath == null){
             throw new IllegalArgumentException("Parameter -DworkDirectory should be passed to the CLI !");
         }
-        if(!new File(workDirectory).exists()){
+        File workDirectory = new File(workDirectoryPath);
+        if(!workDirectory.exists()){
             throw new IllegalArgumentException("Parameter -DworkDirectory should be referencing an existing directory !\");");
+        }
+        if(m2SettingsFilePath == null){
+            throw new IllegalArgumentException("Parameter -Dm2SettingsFile should be passed to the CLI !");
+        }
+        File m2SettingsFile = new File(m2SettingsFilePath);
+        if(!m2SettingsFile.exists()){
+            throw new IllegalArgumentException("Parameter -Dm2SettingsFile should be referencing an existing settings file !\");");
         }
         if(reportFile == null){
             throw new IllegalArgumentException("Parameter -DreportFile should be passed to the CLI !");
         }
 
-        File m2SettingsFile = null;
-        if(m2SettingsFilePath == null || "".equals(m2SettingsFilePath)){
-            m2SettingsFile = new ClassPathResource("m2-settings.xml").getFile();
-        } else {
-            m2SettingsFile = new File(m2SettingsFilePath);
-        }
-
         PluginCompatTesterConfig config = null;
         if(updateCenterUrl != null || (parentCoord != null && !"".equals(parentCoord))){
-            config = new PluginCompatTesterConfig(updateCenterUrl, parentCoord, new File(workDirectory),
+            config = new PluginCompatTesterConfig(updateCenterUrl, parentCoord, workDirectory,
                     new File(reportFile), m2SettingsFile);
         } else {
-            config = new PluginCompatTesterConfig(new File(workDirectory), new File(reportFile), m2SettingsFile);
+            config = new PluginCompatTesterConfig(workDirectory, new File(reportFile), m2SettingsFile);
         }
 
         if(pluginsListParam != null && !"".equals(pluginsListParam)){
