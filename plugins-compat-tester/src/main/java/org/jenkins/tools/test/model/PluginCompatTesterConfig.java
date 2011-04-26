7332a7bbd556494a2f0abd7d827a1c2e5df9643f
Frederic Camblor <fcamblor@gmail.com>
2011-04-26 14:32:40 +0200

    added documentation for config

--- plugins-compat-tester/src/main/java/org/jenkins/tools/test/model/PluginCompatTesterConfig.java
+++ plugins-compat-tester/src/main/java/org/jenkins/tools/test/model/PluginCompatTesterConfig.java
@@ -5,14 +5,32 @@ import java.util.List;
 
 public class PluginCompatTesterConfig {
 
+    // Update center used to retrieve plugins informations
     public final String updateCenterUrl;
+
+    // GroupId which will be used to replace tested plugin's parent groupId
     public final String parentGroupId;
+
+    // ArtifactId which will be used to replace tested plugin's parent artifactId
     public final String parentArtifactId;
+
+    // A working directory where will be checkouted tested plugin's sources
     public final File workDirectory;
+
+    // A report file where will be generated testing report
+    // If the file already exist, testing report will be merged into it
     public final File reportFile;
+
+    // Path for maven settings file where repository will be provided allowing to
+    // download jenkins-core artefact (and dependencies)
     private final File m2SettingsFile;
 
+    // Version which will be used to replace tested plugin's parent verison
+    // If null, latest core version (retrieved via the update center) will be used
     private String parentVersion = null;
+
+    // List of plugin artefact id on which tests will be performed
+    // If null, tests will be performed on every plugins retrieved from update center
     private List<String> pluginsList = null;
 
     public PluginCompatTesterConfig(File workDirectory, File reportFile, File m2SettingsFile){
