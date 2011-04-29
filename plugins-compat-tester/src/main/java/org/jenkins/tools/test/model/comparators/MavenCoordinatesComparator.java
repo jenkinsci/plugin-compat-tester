e471fa25136064d59e233048c7f3b5aba1296e17
Frederic Camblor <fcamblor@gmail.com>
2011-04-29 16:10:16 +0200

    resurrected MavenCoordinatesComparator ... if not present, XStream complains

---
+++ plugins-compat-tester/src/main/java/org/jenkins/tools/test/model/comparators/MavenCoordinatesComparator.java
@@ -0,0 +1,14 @@
+package org.jenkins.tools.test.model.comparators;
+
+import org.jenkins.tools.test.model.MavenCoordinates;
+
+import java.util.Comparator;
+
+/**
+ * @deprecated For backward compatibility only..
+ */
+public class MavenCoordinatesComparator implements Comparator<MavenCoordinates {
+    public int compare(MavenCoordinates o1, MavenCoordinates o2) {
+        return o1.compareTo(o2);
+    }
+}
