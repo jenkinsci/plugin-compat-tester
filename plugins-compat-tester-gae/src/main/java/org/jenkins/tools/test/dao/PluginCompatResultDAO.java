413cf7f7521f499252189415c3451e725a9dcddc
Frédéric Camblor <fcamblor@gmail.com>
2011-11-09 01:29:01 +0100

    Created WritePCTResultServlet which insert a plugin compat tester result into the datastore

---
+++ plugins-compat-tester-gae/src/main/java/org/jenkins/tools/test/dao/PluginCompatResultDAO.java
@@ -0,0 +1,106 @@
+package org.jenkins.tools.test.dao;
+
+import com.google.appengine.api.datastore.*;
+import org.jenkins.tools.test.model.MavenCoordinates;
+import org.jenkins.tools.test.model.PluginCompatResult;
+import org.jenkins.tools.test.model.PluginInfos;
+import org.jenkins.tools.test.model.TestStatus;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ * @author fcamblor
+ */
+public enum PluginCompatResultDAO {
+    INSTANCE;
+
+    private static enum PluginCompatResultProperties {
+        coreCoordsKey, pluginInfosKey, compatTestExecutedOn, status, errorMessage, warningMessages;
+        public static final String KIND = "pluginCompatResult";
+    }
+    private static enum PluginInfosProperties {
+        pluginName, pluginVersion, pluginUrl;
+        public static final String KIND = "pluginInfos";
+    }
+    private static enum MavenCoordinatesProperties {
+        groupId, artifactId, version;
+    }
+    public static final String CORE_MAVEN_COORDS_KIND = "coreCoordinates";
+
+    private static final Logger log = Logger.getLogger(PluginCompatResultDAO.class.getName());
+
+    public void persist(PluginInfos pluginInfos, PluginCompatResult result){
+
+        Key coreCoordinatesEntityKey = createCoreCoordsIfNotExist(result.coreCoordinates);
+        Key pluginInfosEntityKey = createPluginInfosIfNotExist(pluginInfos);
+
+        Entity resultToPersist = new Entity(PluginCompatResultProperties.KIND);
+        resultToPersist.setProperty(PluginCompatResultProperties.coreCoordsKey.name(), coreCoordinatesEntityKey);
+        resultToPersist.setProperty(PluginCompatResultProperties.pluginInfosKey.name(), pluginInfosEntityKey);
+        resultToPersist.setProperty(PluginCompatResultProperties.status.name(), result.status.toString());
+        resultToPersist.setProperty(PluginCompatResultProperties.compatTestExecutedOn.name(), result.compatTestExecutedOn);
+        resultToPersist.setProperty(PluginCompatResultProperties.errorMessage.name(), result.errorMessage);
+        resultToPersist.setProperty(PluginCompatResultProperties.warningMessages.name(), result.warningMessages);
+
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        Key resultKey = datastore.put(resultToPersist);
+
+        log.info("Plugin compat result stored with key : "+resultKey);
+    }
+
+    private Key createPluginInfosIfNotExist(PluginInfos pluginInfos) {
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        List<Entity> pluginInfosEntities = datastore.prepare(
+                new Query(PluginInfosProperties.KIND)
+                    .addFilter(PluginInfosProperties.pluginName.name(), Query.FilterOperator.EQUAL, pluginInfos.pluginName)
+                    .addFilter(PluginInfosProperties.pluginVersion.name(), Query.FilterOperator.EQUAL, pluginInfos.pluginVersion)
+                    .addFilter(PluginInfosProperties.pluginUrl.name(), Query.FilterOperator.EQUAL, pluginInfos.pluginUrl)
+        ).asList(FetchOptions.Builder.withDefaults());
+
+        Key result = null;
+        if(pluginInfosEntities.size() == 0){
+            // Coordinate doesn't exist : let's create it !
+            Entity pluginInfoEntity = new Entity(PluginInfosProperties.KIND);
+            pluginInfoEntity.setProperty(PluginInfosProperties.pluginName.name(), pluginInfos.pluginName);
+            pluginInfoEntity.setProperty(PluginInfosProperties.pluginVersion.name(), pluginInfos.pluginVersion);
+            pluginInfoEntity.setProperty(PluginInfosProperties.pluginUrl.name(), pluginInfos.pluginUrl);
+
+            result = datastore.put(pluginInfoEntity);
+        } else {
+            result = pluginInfosEntities.get(0).getKey();
+        }
+
+        return result;
+    }
+
+    private Key createCoreCoordsIfNotExist(MavenCoordinates coreCoords){
+        return searchAndEventuallyCreateCoords(CORE_MAVEN_COORDS_KIND, coreCoords);
+    }
+
+    private Key searchAndEventuallyCreateCoords(String kind, MavenCoordinates coords){
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        List<Entity> coordsEntities = datastore.prepare(
+                new Query(kind)
+                    .addFilter(MavenCoordinatesProperties.groupId.name(), Query.FilterOperator.EQUAL, coords.groupId)
+                    .addFilter(MavenCoordinatesProperties.artifactId.name(), Query.FilterOperator.EQUAL, coords.artifactId)
+                    .addFilter(MavenCoordinatesProperties.version.name(), Query.FilterOperator.EQUAL, coords.version)
+        ).asList(FetchOptions.Builder.withDefaults());
+
+        Key result = null;
+        if(coordsEntities.size() == 0){
+            // Coordinate doesn't exist : let's create it !
+            Entity coordsEntity = new Entity(kind);
+            coordsEntity.setProperty(MavenCoordinatesProperties.groupId.name(), coords.groupId);
+            coordsEntity.setProperty(MavenCoordinatesProperties.artifactId.name(), coords.artifactId);
+            coordsEntity.setProperty(MavenCoordinatesProperties.version.name(), coords.version);
+
+            result = datastore.put(coordsEntity);
+        } else {
+            result = coordsEntities.get(0).getKey();
+        }
+
+        return result;
+    }
+}
---
+++ plugins-compat-tester-gae/src/main/java/org/jenkins/tools/test/servlets/WritePCTResultServlet.java
@@ -0,0 +1,54 @@
+package org.jenkins.tools.test.servlets;
+
+import org.jenkins.tools.test.dao.PluginCompatResultDAO;
+import org.jenkins.tools.test.model.MavenCoordinates;
+import org.jenkins.tools.test.model.PluginCompatResult;
+import org.jenkins.tools.test.model.PluginInfos;
+import org.jenkins.tools.test.model.TestStatus;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author fcamblor
+ */
+public class WritePCTResultServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Current servlet is secured !
+        SecuritySupport.ensureTokenIsValid(req);
+
+        // Parsing request...
+        String pluginName = req.getParameter("pluginName");
+        String pluginVersion = req.getParameter("pluginVersion");
+        String pluginUrl = req.getParameter("pluginUrl");
+        String[] gav = req.getParameter("mavenGAV").split(":");
+        String groupId = gav[0];
+        String artifactId = gav[1];
+        String version = gav[2];
+        TestStatus status = TestStatus.valueOf(req.getParameter("status"));
+        String errorMessages = req.getParameter("errMsg");
+        List<String> warningMessages = Arrays.asList(req.getParameterValues("warnMsgs"));
+        String dateTimestampStr = req.getParameter("timestamp");
+        Date date = null;
+        if(dateTimestampStr == null){
+            date = new Date(); // If date is not set, use NOW
+        } else {
+            date = new Date(Long.valueOf(dateTimestampStr));
+        }
+
+        PluginInfos pluginInfos = new PluginInfos(pluginName, pluginVersion, pluginUrl);
+        MavenCoordinates mavenCoords = new MavenCoordinates(groupId, artifactId, version);
+        PluginCompatResult result = new PluginCompatResult(mavenCoords, status, errorMessages, warningMessages, null, date);
+
+        // Now, persisting result data into datastore !
+        PluginCompatResultDAO.INSTANCE.persist(pluginInfos, result);
+    }
+}
