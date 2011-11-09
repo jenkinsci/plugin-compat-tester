c9f8745e31d8d1ad9a0c09b0b6e9c67e9a7461c9
Frédéric Camblor <fcamblor@gmail.com>
2011-11-10 00:51:18 +0100

    Refactored ExistingDataImporter in order to be callable from plugins-compat-tester artefact

--- plugins-compat-tester-gae-client/src/main/java/org/jenkins/tools/test/ExistingDataImporter.java
+++ plugins-compat-tester-gae-client/src/main/java/org/jenkins/tools/test/ExistingDataImporter.java
@@ -7,7 +7,6 @@ import org.apache.commons.httpclient.methods.GetMethod;
 import org.jenkins.tools.test.model.PluginCompatReport;
 import org.jenkins.tools.test.model.PluginCompatResult;
 import org.jenkins.tools.test.model.PluginInfos;
-import org.omg.CORBA.NamedValue;
 
 import java.io.File;
 import java.io.IOException;
@@ -21,15 +20,47 @@ import java.util.Map;
 public class ExistingDataImporter {
     public static void main(String[] args) throws IOException {
         File reportFile = new File(args[0]);
-        new ExistingDataImporter().importExistingData(reportFile, args[1], args[2]);
+        new ExistingDataImporter(args[1], args[2]).importExistingReport(reportFile);
     }
 
-    public void importExistingData(File reportFile, String baseGAEUrl, String securityToken) throws IOException {
-        PluginCompatReport report = PluginCompatReport.fromXml(reportFile);
+    private String baseGAEUrl;
+    private String securityToken;
+    public ExistingDataImporter(String baseGAEUrl, String securityToken){
+        this.baseGAEUrl = baseGAEUrl;
+        this.securityToken = securityToken;
+    }
 
+    public void importPluginCompatResult(PluginInfos pluginInfos, PluginCompatResult pluginCompatResult) throws IOException {
         HttpClient httpClient = new HttpClient();
         httpClient.getHttpConnectionManager().getParams().setConnectionTimeout(5000);
         String url = baseGAEUrl+"/writePctResult";
+
+        HttpMethod method = new GetMethod(url);
+        List<NameValuePair> parameters = new ArrayList<NameValuePair>();
+        parameters.add(new NameValuePair("token", securityToken));
+        parameters.add(new NameValuePair("pluginName", pluginInfos.pluginName));
+        parameters.add(new NameValuePair("pluginVersion", pluginInfos.pluginVersion));
+        parameters.add(new NameValuePair("pluginUrl", pluginInfos.pluginUrl));
+        parameters.add(new NameValuePair("mavenGAV", pluginCompatResult.coreCoordinates.toGAV()));
+        parameters.add(new NameValuePair("status", pluginCompatResult.status.name()));
+        if(pluginCompatResult.compatTestExecutedOn != null){
+            parameters.add(new NameValuePair("timestamp", String.valueOf(pluginCompatResult.compatTestExecutedOn.getTime())));
+        }
+        if(pluginCompatResult.errorMessage != null){
+            parameters.add(new NameValuePair("errMsg", pluginCompatResult.errorMessage));
+        }
+        if(pluginCompatResult.warningMessages != null){
+            for(String warnMsg : pluginCompatResult.warningMessages){
+                parameters.add(new NameValuePair("warnMsgs", warnMsg));
+            }
+        }
+        method.setQueryString(parameters.toArray(new NameValuePair[0]));
+
+        httpClient.executeMethod(method);
+    }
+
+    public void importExistingReport(File reportFile) throws IOException {
+        PluginCompatReport report = PluginCompatReport.fromXml(reportFile);
 
         int plannedRequestsCount = 0;
         for (Map.Entry<PluginInfos, List<PluginCompatResult>> test : report.getPluginCompatTests().entrySet()){
@@ -39,29 +70,7 @@ public class ExistingDataImporter {
         int i = 0;
         for (Map.Entry<PluginInfos, List<PluginCompatResult>> test : report.getPluginCompatTests().entrySet()){
             for (PluginCompatResult pluginCompatResult : test.getValue()) {
-                HttpMethod method = new GetMethod(url);
-                List<NameValuePair> parameters = new ArrayList<NameValuePair>();
-                parameters.add(new NameValuePair("token", securityToken));
-                parameters.add(new NameValuePair("pluginName", test.getKey().pluginName));
-                parameters.add(new NameValuePair("pluginVersion", test.getKey().pluginVersion));
-                parameters.add(new NameValuePair("pluginUrl", test.getKey().pluginUrl));
-                parameters.add(new NameValuePair("mavenGAV", pluginCompatResult.coreCoordinates.toGAV()));
-                parameters.add(new NameValuePair("status", pluginCompatResult.status.name()));
-                if(pluginCompatResult.compatTestExecutedOn != null){
-                    parameters.add(new NameValuePair("timestamp", String.valueOf(pluginCompatResult.compatTestExecutedOn.getTime())));
-                }
-                if(pluginCompatResult.errorMessage != null){
-                    parameters.add(new NameValuePair("errMsg", pluginCompatResult.errorMessage));
-                }
-                if(pluginCompatResult.warningMessages != null){
-                    for(String warnMsg : pluginCompatResult.warningMessages){
-                        parameters.add(new NameValuePair("warnMsgs", warnMsg));
-                    }
-                }
-                method.setQueryString(parameters.toArray(new NameValuePair[0]));
-
-                httpClient.executeMethod(method);
-
+                importPluginCompatResult(test.getKey(), pluginCompatResult);
                 i++;
                 System.out.println(String.format("Executed request %d / %d", i, plannedRequestsCount));
             }
