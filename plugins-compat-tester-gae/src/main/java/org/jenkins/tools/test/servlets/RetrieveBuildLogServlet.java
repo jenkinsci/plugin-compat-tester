f8e0da9697849ff815e8a74eccd3d51cc0f39c22
Frédéric Camblor <fcamblor@gmail.com>
2011-11-14 00:08:22 +0100

    Added servlet to retrieve log content

---
+++ plugins-compat-tester-gae/src/main/java/org/jenkins/tools/test/servlets/RetrieveBuildLogServlet.java
@@ -0,0 +1,23 @@
+package org.jenkins.tools.test.servlets;
+
+import org.jenkins.tools.test.dao.LogsDAO;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author fcamblor
+ */
+public class RetrieveBuildLogServlet extends HttpServlet {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String buildLogPath = request.getParameter("buildLogPath");
+
+        String logContent = LogsDAO.INSTANCE.getLogContent(buildLogPath);
+
+        response.getWriter().append(logContent);
+        response.getWriter().flush();
+    }
+}
