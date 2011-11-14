524a3cc74b732ec1b6323ca24176a16f822447f8
Frédéric Camblor <fcamblor@gmail.com>
2011-11-15 00:07:35 +0100

    Added IOUtils allowing to compress / uncompress strings

---
+++ plugins-compat-tester-model/src/test/java/org/jenkins/tools/test/model/utils/IOUtilsTest.java
@@ -0,0 +1,29 @@
+package org.jenkins.tools.test.model.utils;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author fcamblor
+ */
+public class IOUtilsTest {
+
+    @Test
+    public void shouldCompressUncompressBeIdemPotent() throws IOException, ClassNotFoundException {
+        InputStream is = this.getClass().getClassLoader().getResourceAsStream("test.log");
+        String initialContent = IOUtils.streamToString(is);
+
+        String encodedContent = IOUtils.gzipString(initialContent);
+        assertTrue(initialContent.length() > encodedContent.length());
+
+        String decodedContent = IOUtils.gunzipString(encodedContent);
+        assertThat(decodedContent, is(equalTo(initialContent)));
+    }
+}
