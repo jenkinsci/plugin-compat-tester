0d96fb96274c443885e35c7f4611b58b16ff4a49
Josh Soref <jsoref@users.noreply.github.com>
2018-11-25 20:45:22 -0500

    spelling: split

--- plugins-compat-tester-model/src/main/java/org/jenkins/tools/test/model/comparators/VersionComparator.java
+++ plugins-compat-tester-model/src/main/java/org/jenkins/tools/test/model/comparators/VersionComparator.java
@@ -34,34 +34,34 @@ import java.util.Comparator;
 public class VersionComparator implements Comparator<String> {
     public int compare(String o1, String o2) {
 
-        String[] splittedO1Version = o1.split("\\.|-");
-        String[] splittedO2Version = o2.split("\\.|-");
+        String[] splitO1Version = o1.split("\\.|-");
+        String[] splitO2Version = o2.split("\\.|-");
 
-        for(int i=0; i<splittedO1Version.length; i++){
-            if(i >= splittedO2Version.length){
+        for(int i=0; i<splitO1Version.length; i++){
+            if(i >= splitO2Version.length){
                 return 1;
             }
 
             Comparable chunk1 = null;
             try {
-                chunk1 = Integer.valueOf(splittedO1Version[i]);
+                chunk1 = Integer.valueOf(splitO1Version[i]);
             }catch(NumberFormatException e){
-                chunk1 = splittedO1Version[i];
+                chunk1 = splitO1Version[i];
             }
 
             Comparable chunk2 = null;
             try {
-                chunk2 = Integer.valueOf(splittedO2Version[i]);
+                chunk2 = Integer.valueOf(splitO2Version[i]);
             }catch(NumberFormatException e){
-                chunk2 = splittedO2Version[i];
+                chunk2 = splitO2Version[i];
             }
 
-            if(!splittedO1Version[i].equals(splittedO2Version[i])){
+            if(!splitO1Version[i].equals(splitO2Version[i])){
                 return chunk1.compareTo(chunk2);
             }
         }
 
-        if(splittedO1Version.length == splittedO2Version.length){
+        if(splitO1Version.length == splitO2Version.length){
             return 0;
         } else {
             return -1;
