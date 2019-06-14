6471e1d79555be2763cade7393bd9c99f83838c8
Raul Arabaolaza <rarabaolaza@cloudbees.com>
2019-06-14 16:50:30 +0200

    [JENKINS-58051] Hook for plugins using not standard GH tag names

---
+++ plugins-compat-tester/src/main/java/org/jenkins/tools/test/hook/NonStandardTagHook.java
@@ -0,0 +1,94 @@
+package org.jenkins.tools.test.hook;
+
+import hudson.model.UpdateSite;
+import org.apache.maven.scm.ScmFileSet;
+import org.apache.maven.scm.ScmTag;
+import org.apache.maven.scm.command.checkout.CheckOutScmResult;
+import org.apache.maven.scm.manager.ScmManager;
+import org.apache.maven.scm.repository.ScmRepository;
+import org.jenkins.tools.test.SCMManagerFactory;
+import org.jenkins.tools.test.model.PluginCompatTesterConfig;
+import org.jenkins.tools.test.model.PomData;
+import org.jenkins.tools.test.model.hook.PluginCompatTesterHookBeforeCheckout;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * This hook allows plugin using a non standard tag format on github to be checked out.
+ *
+ * The affected plugins are loaded from a properties file whose key is the artifactId and the value is a string to be formatted
+ * for the tag value, this value will be calculated using String.format passing the current version as the only parameter
+ */
+public class NonStandardTagHook  extends PluginCompatTesterHookBeforeCheckout {
+
+    final Properties affectedPlugins;
+    final List<String> transformedPlugins = new LinkedList<>();
+
+    public NonStandardTagHook()  {
+        affectedPlugins = new Properties();
+        try (InputStream inputStream = ClassLoader.getSystemResourceAsStream("nonstandardtagplugins.properties")) {
+            affectedPlugins.load(inputStream);
+            affectedPlugins.keySet().forEach(e -> transformedPlugins.add(e.toString()));
+        } catch (IOException e) {
+            System.err.println("WARNING: NonStandardTagHook was not able to load affected plugins, the hook will do nothing");
+            e.printStackTrace();
+        }
+        ;
+    }
+
+    @Override
+    public boolean check(Map<String, Object> info) throws Exception {
+        PomData pomData = (PomData)info.get("pomData");
+        return affectedPlugins.containsKey(pomData.artifactId);
+    }
+
+    @Override
+    public Map<String, Object> action(Map<String, Object> info) throws Exception {
+        PluginCompatTesterConfig config = (PluginCompatTesterConfig)info.get("config");
+        PomData pomData = (PomData)info.get("pomData");
+        UpdateSite.Plugin plugin = (UpdateSite.Plugin) info.get("plugin");
+
+        // We should not execute the hook if using localCheckoutDir
+        boolean shouldExecuteHook = config.getLocalCheckoutDir() == null || !config.getLocalCheckoutDir().exists();
+
+        if (shouldExecuteHook) {
+            System.out.println("Executing " + this.getClass().getSimpleName() + " for " + pomData.artifactId);
+
+                // Checkout to the parent directory. All other processes will be on the child directory
+                File checkoutPath = new File(config.workDirectory.getAbsolutePath() + "/" + pomData.artifactId);
+
+                String scmTag =  String.format(affectedPlugins.get(pomData.artifactId).toString(), plugin.version);
+                System.out.println("Checking out from SCM tag " + scmTag);
+                ScmManager scmManager = SCMManagerFactory.getInstance().createScmManager();
+                ScmRepository repository = scmManager.makeScmRepository(pomData.getConnectionUrl());
+                CheckOutScmResult result = scmManager.checkOut(repository, new ScmFileSet(checkoutPath), new ScmTag(scmTag));
+
+                if (!result.isSuccess()) {
+                    // Throw an exception if there are any download errors.
+                    throw new RuntimeException(result.getProviderMessage() + "||" + result.getCommandOutput());
+                }
+
+
+            // Checkout already happened, don't run through again
+            info.put("runCheckout", false);
+
+
+            info.put("checkoutDir", checkoutPath);
+            info.put("pluginDir", checkoutPath);
+        }
+        return info;
+
+    }
+
+    @Override
+    public List<String> transformedPlugins() {
+        return transformedPlugins;
+    }
+}
