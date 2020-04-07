5030b8a262f1ae921f30485f4a04a2a2bd3a77dd
imonteroperez <imontero@cloudbees.com>
2020-04-07 11:53:32 +0200

    Enabled incrementals

--- plugins-compat-tester/src/test/java/org/jenkins/tools/test/PluginCompatTesterTest.java
+++ plugins-compat-tester/src/test/java/org/jenkins/tools/test/PluginCompatTesterTest.java
@@ -85,7 +85,7 @@ public class PluginCompatTesterTest {
 
     @Test
     public void testWithUrl() throws Throwable {
-        ImmutableList<String> includedPlugins = ImmutableList.of("accurev"
+        ImmutableList<String> includedPlugins = ImmutableList.of("ant"
         /*
          * "accurev", "active-directory", "analysis-collector", "scm-sync-configuration"
          */
