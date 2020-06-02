0b606d74695700ae26abea3847434594e8598b9b
Francisco Javier Fernandez Gonzalez <fjfernandez@cloudbees.com>
2020-06-02 09:35:40 +0200

    Add hook for jacoco plugin

---
+++ plugins-compat-tester/src/main/java/org/jenkins/tools/test/hook/JacocoHook.java
@@ -0,0 +1,38 @@
+package org.jenkins.tools.test.hook;
+
+import org.jenkins.tools.test.model.PomData;
+import org.jenkins.tools.test.model.hook.PluginCompatTesterHookBeforeExecution;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+/**
+ * Workaround for JaCoCo plugin since it needs execute the jacoco:prepare-agent goal before execution.
+ */
+public class JacocoHook extends PluginCompatTesterHookBeforeExecution {
+
+    @Override
+    public boolean check(Map<String, Object> info) {
+        PomData data = (PomData) info.get("pomData");
+        return "org.jenkins-ci.plugins".equals(data.groupId) && "jacoco".equals(data.artifactId);
+    }
+
+    @Override
+    public Map<String, Object> action(Map<String, Object> info) throws Exception {
+        List<String> args = (List<String>) info.get("args");
+
+        if (args != null) {
+            int index = IntStream.range(0, args.size()).filter(i -> args.get(i).startsWith("hpi:")).findFirst().orElse(-1);
+            if (index == -1) {
+                index = IntStream.range(0, args.size()).filter(i -> args.get(i).equals("surefire:test")).findFirst().orElse(-1);
+            }
+            if (index != -1) {
+                args.add(index, "jacoco:prepare-agent");
+            }
+        }
+
+        return info;
+    }
+
+}
