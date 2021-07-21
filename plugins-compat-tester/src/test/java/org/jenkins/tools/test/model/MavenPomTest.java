593faf8ad36d1ee66e071e326794835ec82168a5
José Lamas <jlr@genexus.com>
2021-07-21 14:29:23 -0300

    Added test for JENKINS-66173
    - make sure to use the right charset while reading xml from resource and when writing it back to a file
    - extended xml string normalization
    - added specific case for a POM using UTF-8 and containing Spanish, Japanese and Chinese characters

--- plugins-compat-tester/src/test/java/org/jenkins/tools/test/model/MavenPomTest.java
+++ plugins-compat-tester/src/test/java/org/jenkins/tools/test/model/MavenPomTest.java
@@ -1,37 +1,40 @@
-
 package org.jenkins.tools.test.model;
 
 import com.google.common.collect.ImmutableMap;
 import hudson.util.VersionNumber;
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
-import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.jvnet.hudson.test.Issue;
 
 public class MavenPomTest {
 
-    @Rule public TemporaryFolder tmp = new TemporaryFolder();
+    @Rule
+    public TemporaryFolder tmp = new TemporaryFolder();
 
     @Issue("https://github.com/jenkinsci/bom/pull/301#issuecomment-694518923")
-    @Test public void addDependenciesWithTestsClassifier() throws Exception {
+    @Test
+    public void addDependenciesWithTestsClassifier() throws Exception {
         File prj = tmp.newFolder();
-        File pom = new File(prj, "pom.xml");
-        FileUtils.copyURLToFile(MavenPomTest.class.getResource("credentials-binding-pom-before.xml"), pom);
-        Map<String,VersionNumber> toAdd = new HashMap<>(Collections.singletonMap("trilead-api", new VersionNumber("1.0.8")));
-        Map<String,VersionNumber> toReplace = new HashMap<>(ImmutableMap.of("credentials", new VersionNumber("2.3.12"), "ssh-credentials", new VersionNumber("1.18.1"), "plain-credentials", new VersionNumber("1.7")));
-        Map<String,VersionNumber> toAddTest = new HashMap<>();
-        Map<String,VersionNumber> toReplaceTest = new HashMap<>(ImmutableMap.of("workflow-scm-step", new VersionNumber("2.11"), "workflow-durable-task-step", new VersionNumber("2.35"), "display-url-api", new VersionNumber("2.3.3"), "script-security", new VersionNumber("1.74"), "workflow-cps", new VersionNumber("2.83")));
+        File pom = createPomFileFromResource(prj, "credentials-binding-pom-before.xml");
+
+        Map<String, VersionNumber> toAdd = new HashMap<>(Collections.singletonMap("trilead-api", new VersionNumber("1.0.8")));
+        Map<String, VersionNumber> toReplace = new HashMap<>(ImmutableMap.of("credentials", new VersionNumber("2.3.12"), "ssh-credentials", new VersionNumber("1.18.1"), "plain-credentials", new VersionNumber("1.7")));
+        Map<String, VersionNumber> toAddTest = new HashMap<>();
+        Map<String, VersionNumber> toReplaceTest = new HashMap<>(ImmutableMap.of("workflow-scm-step", new VersionNumber("2.11"), "workflow-durable-task-step", new VersionNumber("2.35"), "display-url-api", new VersionNumber("2.3.3"), "script-security", new VersionNumber("1.74"), "workflow-cps", new VersionNumber("2.83")));
         toReplaceTest.putAll(ImmutableMap.of("workflow-support", new VersionNumber("3.5"), "workflow-job", new VersionNumber("2.39"), "workflow-basic-steps", new VersionNumber("2.20")));
         VersionNumber coreDep = new VersionNumber("2.164.3");
-        Map<String,String> pluginGroupIds = new HashMap<>(ImmutableMap.of("credentials-binding", "org.jenkins-ci.plugins", "pipeline-build-step", "org.jenkins-ci.plugins", "credentials", "org.jenkins-ci.plugins", "jdk-tool", "org.jenkins-ci.plugins", "snakeyaml-api", "io.jenkins.plugins"));
+        Map<String, String> pluginGroupIds = new HashMap<>(ImmutableMap.of("credentials-binding", "org.jenkins-ci.plugins", "pipeline-build-step", "org.jenkins-ci.plugins", "credentials", "org.jenkins-ci.plugins", "jdk-tool", "org.jenkins-ci.plugins", "snakeyaml-api", "io.jenkins.plugins"));
         pluginGroupIds.putAll(ImmutableMap.of("workflow-step-api", "org.jenkins-ci.plugins.workflow", "plain-credentials", "org.jenkins-ci.plugins", "trilead-api", "org.jenkins-ci.plugins", "command-launcher", "org.jenkins-ci.plugins", "jquery", "org.jenkins-ci.plugins"));
         pluginGroupIds.putAll(ImmutableMap.of("matrix-project", "org.jenkins-ci.plugins", "jquery-detached", "org.jenkins-ci.ui", "ace-editor", "org.jenkins-ci.ui", "git", "org.jenkins-ci.plugins", "workflow-durable-task-step", "org.jenkins-ci.plugins.workflow"));
         pluginGroupIds.putAll(ImmutableMap.of("git-client", "org.jenkins-ci.plugins", "ssh-credentials", "org.jenkins-ci.plugins", "variant", "org.jenkins-ci.plugins", "cloudbees-folder", "org.jenkins-ci.plugins", "scm-api", "org.jenkins-ci.plugins"));
@@ -43,7 +46,50 @@ public class MavenPomTest {
         pluginGroupIds.putAll(ImmutableMap.of("workflow-api", "org.jenkins-ci.plugins.workflow", "git-server", "org.jenkins-ci.plugins"));
         List<String> toConvert = Collections.emptyList();
         new MavenPom(prj).addDependencies(toAdd, toReplace, toAddTest, toReplaceTest, coreDep, pluginGroupIds, toConvert);
-        assertEquals(IOUtils.toString(MavenPomTest.class.getResource("credentials-binding-pom-after.xml")).replaceAll("(?m) +$", ""), FileUtils.readFileToString(pom).replaceAll("(?m) +$", ""));
+
+        assertResourceEqualsXmlFile("credentials-binding-pom-after.xml", pom);
     }
 
+    @Issue("https://github.com/jenkinsci/plugin-compat-tester/pull/287")
+    @Test
+    public void transformPOMwithSpecialCharacters() throws Exception {
+        File folder = tmp.newFolder();
+        File pomFile = createPomFileFromResource(folder, "demo-plugin-pom-before.xml");
+
+        MavenCoordinates coreCoordinates = new MavenCoordinates(
+                "org.jenkins-ci.plugins",
+                "plugin",
+                "2.235.1");
+
+        MavenPom mavenPom = new MavenPom(pomFile.getParentFile());
+        mavenPom.transformPom(coreCoordinates);
+
+        assertResourceEqualsXmlFile("demo-plugin-pom-after.xml", pomFile);
+    }
+
+    private void assertResourceEqualsXmlFile(String resource, File pom) throws IOException {
+        Charset charset = Charset.forName("UTF-8");
+        assertEquals(
+                normalizeXmlString(IOUtils.toString(MavenPomTest.class.getResource(resource), charset)),
+                normalizeXmlString(FileUtils.readFileToString(pom, charset))
+        );
+    }
+
+    private File createPomFileFromResource(File parentFolder, String resource) throws IOException {
+        File pom = new File(parentFolder, "pom.xml");
+        FileUtils.copyURLToFile(MavenPomTest.class.getResource(resource), pom);
+        return pom;
+    }
+
+    private static String normalizeXmlString(String source) {
+        return source
+                // remove leading and trailing spaces
+                .replaceAll("(?m)[ \t]+$", "").replaceAll("(?m)^[ \t] +", "")
+                // normalize new line characters
+                .replaceAll("\r\n", "\n")
+                // ensure new line at end of line
+                .concat("\n")
+                // remove extra new lines
+                .replaceAll("\n\n+", "\n");
+    }
 }
