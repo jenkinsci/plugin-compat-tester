7269bed14a6d0647dd0631dafcaaebb105a0c348
Tim Jacomb <timjacomb1+github@gmail.com>
2022-01-27 20:34:27 +0000

    Handle new JEP-229 versioning

--- plugins-compat-tester/src/test/java/org/jenkins/tools/test/PluginCompatTesterTest.java
+++ plugins-compat-tester/src/test/java/org/jenkins/tools/test/PluginCompatTesterTest.java
@@ -25,6 +25,8 @@
  */
 package org.jenkins.tools.test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
@@ -381,6 +383,15 @@ public class PluginCompatTesterTest {
         fileName = "WEB-INF/lib/jenkins-core-256.0-2090468d82e49345519a2457f1d1e7426f01540b-2090468d82e49345519a2457f1d1e7426f01540b-SNAPSHOT.jar";
         m = Pattern.compile(PluginCompatTester.JENKINS_CORE_FILE_REGEX).matcher(fileName);
         assertTrue("No matches", m.matches());
+    }
+
+    @Test
+    @Issue("340")
+    public void testJEP229WithUnderscore() {
+        String fileName = "WEB-INF/lib/jenkins-core-2.329-rc31964.3b_29e9d46_038_.jar";
+        Matcher m = Pattern.compile(PluginCompatTester.JENKINS_CORE_FILE_REGEX).matcher(fileName);
+        assertThat("No matches", m.matches(), is(true));
+        assertThat("Invalid group", m.group(1), is("2.329-rc31964.3b_29e9d46_038_"));
     }
 
     private static File getSettingsFile() throws IOException {
