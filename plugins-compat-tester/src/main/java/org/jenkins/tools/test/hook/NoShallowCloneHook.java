96551774a52f6a266a7d61cf0985a2a216130e9e
Olivier Lamy <olamy@apache.org>
2022-11-18 14:20:35 +1000

    prefer a finished list of shallow plugins
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- plugins-compat-tester/src/main/java/org/jenkins/tools/test/hook/NoShallowCloneHook.java
+++ plugins-compat-tester/src/main/java/org/jenkins/tools/test/hook/NoShallowCloneHook.java
@@ -4,15 +4,20 @@ import org.apache.commons.lang.StringUtils;
 import org.jenkins.tools.test.PluginCompatTester;
 import org.jenkins.tools.test.model.hook.PluginCompatTesterHookBeforeCheckout;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 public class NoShallowCloneHook extends PluginCompatTesterHookBeforeCheckout {
+
+
+    private static final List<String> NO_SHALLOW_PLUGIN = Arrays.asList("git");
 
     @Override
     public Map<String, Object> action(Map<String, Object> moreInfo) throws Exception {
         String pluginName = (String) moreInfo.get("pluginName");
         // such plugin need commit history for some tests
-        if (StringUtils.startsWith(pluginName, "git")) {
+        if (NO_SHALLOW_PLUGIN.contains(pluginName)) {
             System.out.println("Disable shallow clone for plugin:" + pluginName);
             moreInfo.put(PluginCompatTester.SHALLOW_CLONE, false);
         }
