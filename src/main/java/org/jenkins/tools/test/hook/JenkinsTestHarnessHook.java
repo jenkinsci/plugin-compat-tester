589700b88144559752f258d216b14cc190452158
Basil Crow <me@basilcrow.com>
2023-03-27 18:47:55 -0700

    Require a minimum version of the Jenkins Test Harness

---
+++ src/main/java/org/jenkins/tools/test/hook/JenkinsTestHarnessHook.java
@@ -0,0 +1,22 @@
+package org.jenkins.tools.test.hook;
+
+import org.jenkins.tools.test.model.hook.PluginCompatTesterHookBeforeExecution;
+import org.kohsuke.MetaInfServices;
+
+/**
+ * Recent core versions require a relatively recent Jenkins Test Harness, so ensure that a minimum
+ * supported version is in place.
+ */
+@MetaInfServices(PluginCompatTesterHookBeforeExecution.class)
+public class JenkinsTestHarnessHook extends PropertyVersionHook {
+
+    @Override
+    public String getProperty() {
+        return "jenkins-test-harness.version";
+    }
+
+    @Override
+    public String getMinimumVersion() {
+        return "1903.vf505ecb_63589";
+    }
+}
