2576d232d1159fbd24d3d0a9c39ed52c1d1ee3c5
Basil Crow <me@basilcrow.com>
2023-04-11 17:08:08 -0700

    Spotless

--- src/main/java/org/jenkins/tools/test/PluginCompatTester.java
+++ src/main/java/org/jenkins/tools/test/PluginCompatTester.java
@@ -36,7 +36,6 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
