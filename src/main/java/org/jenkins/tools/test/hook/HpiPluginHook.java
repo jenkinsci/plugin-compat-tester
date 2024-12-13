951c5d0cb5e40b0ac476d87cd23baacd8fe3e10e
Jesse Glick <jglick@cloudbees.com>
2024-12-13 13:46:39 -0500

    Pick up `isPlugin` fix

--- src/main/java/org/jenkins/tools/test/hook/HpiPluginHook.java
+++ src/main/java/org/jenkins/tools/test/hook/HpiPluginHook.java
@@ -4,12 +4,7 @@ import org.jenkins.tools.test.model.hook.PluginCompatTesterHookBeforeExecution;
 import org.kohsuke.MetaInfServices;
 
 /**
- * The {@code overrideWar} option is available in HPI Plugin 3.29 or later, but many plugins under
- * test still use an older plugin parent POM and therefore an older HPI plugin version.
- * Additionally the code did not work correctly with SNAPSHOT versions until HPI Plugin version 3.44.
- * As a temporary workaround, we override the HPI plugin version to a recent version. When all plugins in
- * the managed set are using a plugin parent POM with HPI Plugin 3.44 or later (i.e., plugin parent
- * POM 4.63 or later), this can be deleted.
+ * Picking up mojo fixes not necessarily in existing plugin releases.
  */
 @MetaInfServices(PluginCompatTesterHookBeforeExecution.class)
 public class HpiPluginHook extends PropertyVersionHook {
@@ -21,6 +16,6 @@ public class HpiPluginHook extends PropertyVersionHook {
 
     @Override
     public String getMinimumVersion() {
-        return "3.44";
+        return "3.60-20241213.184504-1";
     }
 }
